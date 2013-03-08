9cb3b2aa20619dca88c1e34da19fbc9251823435
Anton Evdokimov <free0u@gmail.com>
2013-03-08 20:00:50 +0400

    Solve task31

--- task31/solve.cpp
+++ task31/solve.cpp
@@ -6,9 +6,25 @@
 
 using namespace std;
 
+int values[] = {1, 2, 5, 10, 20, 50, 100, 200};
+
+int foo(int cur_cnt, int m = 0) {
+	if (cur_cnt == 0) return 1;
+	if (cur_cnt < 0) return 0;
+
+	int res = 0;
+	for (int i = m; i < 8; ++i) {
+		res += foo(cur_cnt - values[i], i);
+	}
+
+	return res;
+}
+
 int main() {
 	freopen("input.txt", "r", stdin);
 	freopen("output.txt", "w", stdout);
+
+	cout << foo(200);
 
 	return 0;
 }
