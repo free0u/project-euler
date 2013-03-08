9d108171f2a8d2fb78cf5daf230442a411d00b70
Anton Evdokimov <free0u@gmail.com>
2013-03-09 00:39:36 +0400

    Add skeleton for task37

---
+++ task37/solve.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstdio>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+
+	return 0;
+}
